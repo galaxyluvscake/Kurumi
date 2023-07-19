01f5af2945d3dbe2eaa82a18dfc4a949e8f742a9
galaxyluvscake <126529020+galaxyluvscake@users.noreply.github.com>
2023-07-19 18:15:10 +0100

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,20 +2,20 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "22548132639";   // You can replace this number with yours //
 }
 
 
-global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
-global.sessionId = process.env.SESSION_ID || "ok";
-global.prefa = process.env.PREFIX || "-";
+global.owner = gg.split("22548132639, 237658679320, 212774518801");
+global.mongodb = process.env.MONGODB || "mongodb+srv://atlas:blin6@cluster0.3pfvqv5.mongodb.net/?retryWrites=true&w=majority";
+global.sessionId = process.env.SESSION_ID || "amb117415";
+global.prefa = process.env.PREFIX || "!";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas Bot`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `meme`;
+global.author = process.env.AUTHOR || "nigger";
 global.port = process.env.PORT || "10000";
-global.openAiAPI = process.env.OPENAI_API || "Put your openai API key here";
-global.owner = gg.split(",");
+global.openAiAPI = process.env.OPENAI_API || "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";
+global.owner = gg.split("22548132639, 237658679320, 212774518801");
 
 module.exports = {
   mongodb: global.mongodb,
